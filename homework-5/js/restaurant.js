a879b2ed9103e7c192dc17cb02a7eed87ca2ff74
csalguera <carlos.e.salguera@gmail.com>
2025-03-04 13:27:22 -0500

    add elements to html and define variables to reference them

---
+++ homework-5/js/restaurant.js
@@ -0,0 +1,11 @@
+// cached element references
+const menuRef = document.querySelector('#menu')
+const btnRef = document.querySelector('#remove-item')
+const pRef = document.querySelector('#no-items')
+
+const menuItems = [
+  'Hamburger',
+  'Cheeseburger',
+  'Fries',
+  'Onion Rings',
+]
