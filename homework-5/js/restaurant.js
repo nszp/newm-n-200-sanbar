e26d1e4e15652c523c6efb71c8ebd42d85163c3d
csalguera <carlos.e.salguera@gmail.com>
2025-03-04 14:08:35 -0500

    add functionality to restaurant.js file

--- homework-5/js/restaurant.js
+++ homework-5/js/restaurant.js
@@ -1,6 +1,7 @@
 // cached element references
 const menuRef = document.querySelector('#menu')
 const btnRef = document.querySelector('#remove-item')
+const cartRef = document.querySelector('#cart')
 const pRef = document.querySelector('#no-items')
 
 const menuItems = [
@@ -9,3 +10,48 @@ const menuItems = [
   'Fries',
   'Onion Rings',
 ]
+
+const cart = []
+
+const showP = () => {
+  if (!cart.length) {
+    pRef.style.display = 'inline'
+  } else {
+    pRef.style.display = 'none'
+  }
+}
+
+const addItemToCart = (evt) => {
+  cart.push(evt.target.dataset.item)
+  showCart()
+}
+
+const removeItemFromCart = () => {
+  cart.pop()
+  showCart()
+}
+
+const showCart = () => {
+  cartRef.innerHTML = ''
+  for (let i = 0; i < cart.length; i++) {
+    cartRef.innerHTML += `<li>${cart[i]}</li>`
+  }
+  showP()
+}
+
+for (let i = 0; i < menuItems.length; i++) {
+  const item = menuItems[i]
+  const liEl = document.createElement('li')
+  liEl.innerText = item
+  const btnEl = document.createElement('button')
+  btnEl.innerText = '+'
+  btnEl.style.width = '24px'
+  btnEl.style.height = '24px'
+  btnEl.style.margin = '0 0 8px 8px'
+  btnEl.dataset.item = item
+  btnEl.onclick = addItemToCart
+  liEl.appendChild(btnEl)
+  menuRef.appendChild(liEl)
+}
+
+btnRef.addEventListener('click', removeItemFromCart)
