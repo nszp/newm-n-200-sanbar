54aee5a7c8da6b9674e0219c694f0e177ad69fbd
csalguera <carlos.e.salguera@gmail.com>
2025-04-28 16:27:48 -0400

    add css file

--- final/js/olympians.js
+++ final/js/olympians.js
@@ -1,15 +1,20 @@
 import { getResources } from "./services.js";
 
+const containerRef = document.querySelector('#container')
+
 async function renderOlympians() {
   const olympians = await getResources("olympians")
 
   olympians.forEach(olympian => {
     const name = document.createElement('h2')
     const img = document.createElement('img')
+    const div = document.createElement('div')
     name.textContent = olympian.greekName
     img.src = olympian.image
-    document.body.appendChild(name)
-    document.body.appendChild(img)
+    div.id = 'card'
+    div.appendChild(name)
+    div.appendChild(img)
+    containerRef.append(div)
   });
 }
 
