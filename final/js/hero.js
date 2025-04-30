f3562ab0f2f4fec020b7b9dbabc1823ef70bd0b5
Nnaemeka <kristopheronochie@gmail.com>
2025-04-29 22:07:55 -0400

    update

--- final/js/hero.js
+++ final/js/hero.js
@@ -1,11 +1,11 @@
-import { getResources } from './services.js';
+import { getResource } from './services.js';
 
 const params       = new URLSearchParams(window.location.search);
 const heroId       = params.get('id');
 const containerRef = document.querySelector('#hero-details');
 
 async function renderHero() {
-    const hero = await getResources(`hero/${heroId}`);
+    const hero = await getResource(`hero/${heroId}`);
     const title = document.createElement('h2');
     const img = document.createElement('img');
     const description = document.createElement('p');
