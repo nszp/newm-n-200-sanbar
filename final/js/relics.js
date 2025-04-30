0358592c2963b43925305be6e9a85604883cdc72
Alonso Palmero <palmeroa@iu.edu>
2025-04-29 22:14:43 -0400

    added styling for relics.html,

--- final/js/relics.js
+++ final/js/relics.js
@@ -1,15 +1,28 @@
 import { getResources } from "./services.js";
 
+const containerRef = document.querySelector("#container");
+
 async function renderRelics() {
   const relics = await getResources("relics");
 
   relics.forEach((relics) => {
     const name = document.createElement("h2");
     const img = document.createElement("img");
+    const card = document.createElement("div");
+    const link = document.createElement("a");
     name.textContent = relics.name;
     img.src = relics.image;
-    document.body.appendChild(name);
-    document.body.appendChild(img);
+    card.className = "card";
+    link.href = `relic.html/?id=${relics.id}`;
+    link.className = "card-link";
+    link.textContent = "Learn More";
+    img.className = "relics-img";
+    card.appendChild(name);
+    card.appendChild(img);
+    card.appendChild(link);
+    // document.body.appendChild(name);
+    // document.body.appendChild(img);
+    containerRef.append(card);
   });
 }
 
