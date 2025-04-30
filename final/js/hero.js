9831b8a11929bc38cd64c1c6ddb9276681fdc032
Nnaemeka Onochie <118133266+krisono@users.noreply.github.com>
2025-04-29 21:09:54 -0400

    Create hero.js

---
+++ final/js/hero.js
@@ -0,0 +1,22 @@
+import { getResources } from './services.js';
+
+const params       = new URLSearchParams(window.location.search);
+const heroId       = params.get('id');
+const containerRef = document.querySelector('#hero-details');
+
+async function renderHero() {
+    const hero = await getResources('heroes/${heroId}');
+    const title = document.createElement('h2');
+    const img = document.createElement('img');
+    const description = document.createElement('p');
+    const link = document.createElement('a');
+    title.textContent = hero.name;
+    img.src = hero.image;
+    img.alt = hero.name;
+    description.textContent = hero.description || hero.bio || '';
+    containerRef.appendChild(title);
+    containerRef.appendChild(img);
+    containerRef.appendChild(desc);
+}
+
+renderHero();
